7808ed516ccdedb8953660edeba7649153213df2
Gunil <gunil.na1@gmail.com>
2021-02-07 15:41:17 +0900

    refactoring to multiple plugins

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,51 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: inputCode,
+          };
+        }
+
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        // if it is, return it immediately
+        // 이미 이 파일을 fetch 했는지, 그리고 캐시에 있는지 확인하고
+        // 있다면 바로 반환
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        // store response in cache
+        // 캐시에 응답을 저장
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
