057852f84cf8e1acc165bd605da303059cd7ca91
Gunil <gunil.na1@gmail.com>
2021-02-07 18:23:35 +0900

    extracting common caching logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -18,8 +18,8 @@ export const fetchPlugin = (inputCode: string) => {
         };
       });
 
-      // css 파일일떄
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      // 모든 파일에 대해
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // Check to see if we have already fetched this file
         // and if it is in the cache
         // if it is, return it immediately
@@ -32,7 +32,10 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      // css 파일일떄
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const escaped = data
@@ -61,21 +64,6 @@ export const fetchPlugin = (inputCode: string) => {
 
       // 나머지 js파일
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        // if it is, return it immediately
-        // 이미 이 파일을 fetch 했는지, 그리고 캐시에 있는지 확인하고
-        // 있다면 바로 반환
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
