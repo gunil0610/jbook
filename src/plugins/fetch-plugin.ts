f415254d40a64d818889b8d040ee06c67973581b
Gunil <gunil.na1@gmail.com>
2021-02-07 18:17:19 +0900

    separate loading filters

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,15 +10,58 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      // index.js 일때
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
+      // css 파일일떄
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        // if it is, return it immediately
+        // 이미 이 파일을 fetch 했는지, 그리고 캐시에 있는지 확인하고
+        // 있다면 바로 반환
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // store response in cache
+        // 캐시에 응답을 저장
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      // 나머지 js파일
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
 
         // Check to see if we have already fetched this file
         // and if it is in the cache
@@ -35,26 +78,9 @@ export const fetchPlugin = (inputCode: string) => {
 
         const { data, request } = await axios.get(args.path);
 
-        // path 가 css파일이면 fileType을 css로, 아니면 jsx로 설정
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
-        const escaped = data
-          .replace(/\n/g, '')
-          .replace(/"/g, '\\"')
-          .replace(/'/g, "\\'");
-
-        const contents =
-          fileType === 'css'
-            ? `
-          const style = document.createElement('style');
-          style.innerText = '${escaped}';
-          document.head.appendChild(style);
-        `
-            : data;
-
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents,
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
