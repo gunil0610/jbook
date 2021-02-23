ca4aaeba315dbb695b557be446e2fb60790d3122
Gunil <gunil.na1@gmail.com>
2021-02-24 03:27:07 +0900

    resizing horizontally

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -7,7 +7,9 @@ interface PreviewProps {
 
 const html = `
 <html>
-  <head></head>
+  <head>
+    <style>html { background-color: white; }</style>
+  </head>
   <body>
     <div id='root'></div>
     <script>
@@ -36,7 +38,6 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   return (
     <div className="preview-wrapper">
       <iframe
-        style={{ backgroundColor: 'white' }}
         ref={iframe}
         title="preview"
         srcDoc={html}
